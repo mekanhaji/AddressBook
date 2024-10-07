c2fdc96b24e47204175fdd266fb1cefba56437c4
kanhaji <myself.kanhaji@gmail.com>
2024-10-08 02:11:49 +0530

    feat: created tables

--- sql/init.sql
+++ sql/init.sql
@@ -9,24 +9,21 @@ CREATE TABLE users (
 
 INSERT INTO users (id, username, password) VALUES ( 0, 'admin', 'admin');
 
-CREATE TABLE tags (
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(100) NOT NULL,
-    user_id INT NOT NULL DEFAULT 1,
-
-    FOREIGN KEY (user_id) REFERENCES users(id)
-        ON DELETE CASCADE
+CREATE TABLE contacts (
+  id INT PRIMARY KEY,
+  contact_number INT NOT NULL CHECK (LENGTH(contact_number) = 10),
+  email VARCHAR(50) NOT NULL CHECK (email REGEXP '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'),
+  user_id INT NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
-INSERT INTO tags (name) VALUES ('FAVORITE');
-INSERT INTO tags (name) VALUES ('FRIEND');
-INSERT INTO tags (name) VALUES ('FAMILY');
-
-CREATE TABLE contacts (
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(100) NOT NULL,
-    address VARCHAR(255) NOT NULL,
-    user_id INT NOT NULL,
-
-    FOREIGN KEY (user_id) REFERENCES users(id)
+CREATE TABLE address (
+  id INT PRIMARY KEY,
+  building_number VARCHAR(10) NOT NULL,
+  street_name VARCHAR(20),
+  state VARCHAR(20) NOT NULL,
+  pincode INT NOT NULL CHECK (LENGTH(pincode) = 6),
+  country VARCHAR(20) NOT NULL,
+  contact_id INT NOT NULL,
+  FOREIGN KEY (contact_id) REFERENCES contacts(id)
 );
